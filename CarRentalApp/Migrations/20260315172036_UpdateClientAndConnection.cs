fb906ef3b80ade402dcc84f4afbe4fc0621dcdb6
Kamil Biolka <kamilbiolka@interia.pl>
2026-03-15 18:24:17 +0100

    Zmieniłem connection string na localhost

---
+++ CarRentalApp/Migrations/20260315172036_UpdateClientAndConnection.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarRentalApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateClientAndConnection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Seats",
+                table: "Cars");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Segment",
+                table: "Cars",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Mileage",
+                table: "CarFleets",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Segment",
+                table: "Cars");
+
+            migrationBuilder.DropColumn(
+                name: "Mileage",
+                table: "CarFleets");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Seats",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
